41b251a161705772f39c4b916bcaa1717d621e2f
Łukasz <ukidabek@gmail.com>
2022-03-27 18:56:49 +0200

    [Code] BoolEvent added

---
+++ Assets/_Code/Events/BoolEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Logic.Events
+{
+    [CreateAssetMenu(fileName = "BoolEvent.asset", menuName = "Events/BoolEvent")]
+    public class BoolEvent : EventBase<bool>
+    {
+    }
+}
